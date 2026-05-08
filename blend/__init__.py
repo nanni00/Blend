539423273610ffc764055e42c4778210ff08ed73
nanni00 <giovanni.malaguti00@gmail.com>
2026-05-08 16:45:10 +0200

    index_tables_seq available

--- blend/__init__.py
+++ blend/__init__.py
@@ -1,4 +1,4 @@
 from .blend import BLEND
-from .indexing import index_tables
+from .indexing import index_tables, index_tables_seq
 
-__all__ = ["BLEND", "index_tables"]
+__all__ = ["BLEND", "index_tables", "index_tables_seq"]
